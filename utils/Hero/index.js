692c20f7febb542392fc14d8f80a75d76be4f2a2
Rishi Karri <rishikarri1@gmail.com>
2018-10-27 17:20:48 -0400

    confirmed babel is working

--- utils/Hero/index.js
+++ utils/Hero/index.js
@@ -1,5 +1,8 @@
 export function hero () {
     console.log('hero');
     console.log('if this wokr asdf');
-    console.log('asoidjfoaisdjf')
+    console.log('asoidjfoaisdjf');
+    console.log('ypu');
+    const array = [1, 2, 3, 4]
+    console.log(2,...array);
 }
