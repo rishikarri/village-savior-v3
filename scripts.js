f74b9236580416f2a842f4075e59280de5e8fa59
Rishi Karri <rishikarri1@gmail.com>
2018-10-26 20:14:48 -0400

    background image on screen

--- scripts.js
+++ scripts.js
@@ -2,14 +2,27 @@
 
 $( document ).ready(function() {
     // creating a canvas element
-    var canvas = document.createElement("canvas"); 
+    const canvas = document.createElement("canvas"); 
     var context = canvas.getContext("2d");
     // set the canvas height and width 
     canvas.width = 675;
     canvas.height = 480; 
-
-    // document.getElementById("canvas").appendChild(canvas);
-
     $("#canvas").append(canvas);
+    
+    var backgroundImage = new Image(); 
+    backgroundImage.src = "./Images/background.jpeg";
+
+    
+
+   function draw() {
+        console.log(backgroundImage, 'background image')
+        console.log(context, 'cont3ext');
+        context.drawImage(backgroundImage, 0, 0); // @TODO: move this to be background in plain css and then simply draw characters on constant
+   }
+
+    backgroundImage.onload = function() {
+        draw();
+    }
+   
     
 });
