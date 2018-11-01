bb1ade91b43a69c5edd3391d7966be7ffb182353
Rishi Karri <rishikarri1@gmail.com>
2018-10-31 20:05:06 -0400

    move using wsad

--- utils/Hero/index.js
+++ utils/Hero/index.js
@@ -11,22 +11,28 @@ export function Hero(name, image, speed) {
   this.y = 200;
 
   this.move = function(keysPressed) {
-    if (37 in keysPressed) {
+    // move left
+    if (65 in keysPressed) {
       if (this.x >= 80) {
         this.x -= 7 * this.speed;
       }
     }
-    if (38 in keysPressed) {
+    // move up 
+    if (87 in keysPressed) {
       if (this.y >= 30) {
         this.y -= 7 * this.speed;
       }
     }
-    if (39 in keysPressed) {
+
+    // move right
+    if (68 in keysPressed) {
       if (this.x <= 520) {
         this.x += 7 * this.speed;        
       }
     }
-    if (40 in keysPressed) {
+
+    // move down
+    if (83 in keysPressed) {
       if (this.y <= 390) {
         this.y += 7 * this.speed;
       }
