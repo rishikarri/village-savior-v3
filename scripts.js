0451ee92665161953b68718c6f7244e284239dd1
Rishi Karri <rishikarri1@gmail.com>
2018-10-31 20:03:04 -0400

    archer can move

--- scripts.js
+++ scripts.js
@@ -1,31 +1,40 @@
-import { Hero } from './utils/Hero/index.js';
+import { Hero } from "./utils/Hero/index.js";
 
-$( document ).ready(function() {
-    // creating a canvas element
-    const canvas = document.createElement("canvas"); 
-    var context = canvas.getContext("2d");
-    // set the canvas height and width 
-    canvas.width = 675;
-    canvas.height = 480; 
-    $("#canvas").append(canvas);
+$(document).ready(function() {
+  // creating a canvas element
+  const canvas = document.createElement("canvas");
+  var context = canvas.getContext("2d");
+  // set the canvas height and width
+  canvas.width = 675;
+  canvas.height = 480;
+  $("#canvas").append(canvas);
+
+  var backgroundImage = new Image();
+  backgroundImage.src = "./Images/background.jpeg";
+
+  var keysPressed = []; //array that holds whats in the array
+
+  addEventListener("keyup", function(event) {
+    delete keysPressed[event.keyCode];
+  });
+  //anonymous functions to pass moveArcher commands
+  addEventListener("keydown", function(event) {
+    keysPressed[event.keyCode] = true; //this position of the array has a position of true
+  });
+  const archer = new Hero("archer", "./Images/archer.png", 1);
+
+  function draw() {
+    context.drawImage(backgroundImage, 0, 0); // @TODO: move this to be background in plain css and then simply draw characters on constant
+    context.drawImage(archer.image, archer.x, archer.y);
+    archer.move(keysPressed);
+    // create loop so that we keep drawing 
+    requestAnimationFrame(draw);
+  }
+
+  backgroundImage.onload = function() {
+    console.log(archer, "archer");
+
+    draw();
     
-    var backgroundImage = new Image(); 
-    backgroundImage.src = "./Images/background.jpeg";
-
-    const archer = new Hero('archer', './Images/archer.png', 1);
-
-    function draw() {
-        
-        context.drawImage(backgroundImage, 0, 0); // @TODO: move this to be background in plain css and then simply draw characters on constant
-        context.drawImage(archer.image, archer.x, archer.y);
-    }
-
-    backgroundImage.onload = function() {
-        console.log(archer, 'archer');
-
-
-        draw();
-    }
-   
-    
+  };
 });
--- utils/Hero/index.js
+++ utils/Hero/index.js
@@ -1,8 +1,35 @@
-export function Hero (name, image, speed) {
-    this.name = name; 
-    this.image = new Image();
-    this.image.src = image; 
-    this.speed = speed; 
-    this.x = 200; 
-    this.y = 200; 
+// CREATE EVENT LISTENERS
+
+
+
+export function Hero(name, image, speed) {
+  this.name = name;
+  this.image = new Image();
+  this.image.src = image;
+  this.speed = speed;
+  this.x = 200;
+  this.y = 200;
+
+  this.move = function(keysPressed) {
+    if (37 in keysPressed) {
+      if (this.x >= 80) {
+        this.x -= 7 * this.speed;
+      }
+    }
+    if (38 in keysPressed) {
+      if (this.y >= 30) {
+        this.y -= 7 * this.speed;
+      }
+    }
+    if (39 in keysPressed) {
+      if (this.x <= 520) {
+        this.x += 7 * this.speed;        
+      }
+    }
+    if (40 in keysPressed) {
+      if (this.y <= 390) {
+        this.y += 7 * this.speed;
+      }
+    }
+  };
 }
