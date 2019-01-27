fe2a339875a6d300a2ff48c9c4ac560e53626c45
Rishi Karri <rishikarri1@gmail.com>
2019-01-27 09:58:39 -0500

    create first goblin after 5 sec

--- modules/EnemyInterval/index.js
+++ modules/EnemyInterval/index.js
@@ -1,23 +1,20 @@
 import { Goblin } from "../../modules/Goblin/index.js";
 
-var enemyArr = [];
-
+const enemyArr = [];
 let prevTime = 0;
+
 function createEnemies(archer) {
   const time = Number(document.getElementById("timer").innerHTML);
-  console.log(time);
-
- 
-
   // time is going to update several times a second - only run code when there is an increment
-  if (time !== prevTime) {
-    if (time % 5 == 0) {
-      const goblin = Object.create(Goblin);
-      goblin.setUp(archer);
-      enemyArr.push(goblin);
-    }
-    prevTime = time;
+
+  if (time === prevTime) return;
+
+  if (prevTime % 5 == 0 && prevTime !== 0) {
+    const goblin = Object.create(Goblin);
+    goblin.setUp(archer);
+    enemyArr.push(goblin);
   }
+  prevTime = Number(time);
 }
 
 export {
